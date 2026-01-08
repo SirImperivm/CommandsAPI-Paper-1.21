9451e49c842aace2e652a8b404acea2894545cf4
SirImperivm <riccardo.chimento24@gmail.com>
2026-01-08 23:52:29 +0100

    feat: add detailed documentation for ArgType enum definition

--- src/main/java/me/sirimperivm/commandsAPI/command/model/ArgType.java
+++ src/main/java/me/sirimperivm/commandsAPI/command/model/ArgType.java
@@ -1,5 +1,17 @@
 package me.sirimperivm.commandsAPI.command.model;
 
+/**
+ * Represents the type of argument. This enum defines the possible data types
+ * that an argument can be, helping to define and enforce type restrictions in
+ * commands or operations that require specific input formats.
+ *
+ * The types included are:
+ * - STRING: Represents a textual value.
+ * - INTEGER: Represents a 32-bit signed integer.
+ * - LONG: Represents a 64-bit signed integer.
+ * - DOUBLE: Represents a floating-point value.
+ * - BOOLEAN: Represents a true or false value.
+ */
 public enum ArgType {
 
     STRING,
